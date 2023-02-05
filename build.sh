2d4a2cca7e59ff3cacca7de31700ebed573d62dd
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-04 23:43:00 -0500

    Script build completed

--- build.sh
+++ build.sh
@@ -3,13 +3,19 @@
 microservices=("config-server" "eureka-server" "gateway" "customer-microservice" "movement-microservice" "passive-microservice")
 cd ..
 
+# Profile
 read -p "Do you want to change the active profile? (Y/N) " answer
 
 if [ "$answer" == "Y" ] || [ "$answer" == "y" ]; then
   read -p "Enter the value for the active profile (dev, prod, or docker): " value
   if [ "$value" == "dev" ] || [ "$value" == "prod" ] || [ "$value" == "docker" ]; then
-    for file in $(find . -name bootstrap.yml); do
-      sed -i "5s/active:.*/active: $value/" $file
+    for microservice in "${microservices[@]}"; do
+      if [ -d "$microservice" ]; then
+        bootstrap_file="$microservice/src/main/resources/bootstrap.yml"
+        if [ -f "$bootstrap_file" ]; then
+          sed -i "5s/active:.*/active: $value/" "$bootstrap_file"
+        fi
+      fi
     done
   else
     echo "Invalid value entered. Exiting script."
@@ -17,25 +23,28 @@ if [ "$answer" == "Y" ] || [ "$answer" == "y" ]; then
   fi
 fi
 
+#Jar
 read -p "Do you want to generate the jar files? (Y/N) " answer
 if [ "$answer" == "Y" ] || [ "$answer" == "y" ]; then
   # Compile JARs for each microservice in parallel
   for microservice in "${microservices[@]}"; do
-    (
-      cd $microservice
-      mvn clean install
-    ) &
+    if [ -d "$microservice" ]; then
+      (
+        cd "$microservice"
+        mvn clean install
+      ) &
+    fi
   done
 
   wait
+fi
 
+#Containers
+read -p "Do you want to start the Docker containers? (Y/N) " answer
+if [ "$answer" == "Y" ] || [ "$answer" == "y" ]; then
   # Start the Docker containers
+  cd ./config-server-directory
   docker-compose up --build
-
-  end_time=$(date +%s)
-  total_time=$((end_time - start_time))
-
-  echo "Total time taken: $total_time seconds."
 else
-  echo "Exiting without generating jar files."
+  echo "Exiting without starting Docker containers"
 fi
